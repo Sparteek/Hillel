21b5959570fc494b8c29cd88a5e814e5dd5200a1
spart-lab <sergii.riezanov.pe@gmail.com>
2026-06-23 23:00:40 +0300

    Add LN 13

---
+++ Lesson_notes/Lesson_13/Try_HW.py
@@ -0,0 +1,104 @@
+"""
+Ваша команда та ви розробляєте систему входу для веб-додатка,
+і вам потрібно реалізувати тести на функцію для логування подій в системі входу.
+Дано функцію, напишіть набір тестів для неї.
+"""
+
+import logging
+
+def log_event(username: str, status: str):
+    """
+    Логує подію входу в систему.
+
+    username: Ім'я користувача, яке входить в систему.
+
+    status: Статус події входу:
+
+    * success - успішний, логується на рівні інфо
+    * expired - пароль застаріває і його слід замінити, логується на рівні warning
+    * failed  - пароль невірний, логується на рівні error
+    """
+    log_message = f"Login event - Username: {username}, Status: {status}"
+
+    # Створення та налаштування логера
+    logging.basicConfig(
+        filename='login_system.log',
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s',
+        force = True
+        )
+    logger = logging.getLogger("log_event")
+
+    # Логування події
+    if status == "success":
+        logger.info(log_message)
+    elif status == "expired":
+        logger.warning(log_message)
+    else:
+        logger.error(log_message)
+
+def get_message(user,  status):
+        return f"Login event - Username: {user}, Status: {status}"
+
+# def test_log_event_success():
+#     log_event("user1", "success")
+#     with open("login_system.log") as file_log:
+#         logs = file_log.read()
+#     message = get_message("user1", "success")
+#     assert message in logs
+#
+# def test_log_event_expired():
+#     log_event("user2", "expired")
+#     with open("login_system.log") as file_log:
+#         logs = file_log.read()
+#     message = get_message("user2", "expired")
+#     assert message in logs
+#
+# def test_log_event_failed():
+#     log_event("user3", "failed")
+#     with open("login_system.log") as file_log:
+#         logs = file_log.read()
+#         message = get_message("user3", "failed")
+#         assert message in logs
+def read_logs(file_log):
+    with open(file_log, "r") as file_log:
+        return file_log.read()
+
+def clear_log_file():
+    open("login_system.log", "w").close()
+
+def check_log_event(user, status):
+    log_event(user, status)
+    logs = read_logs("login_system.log")
+    message = get_message(user, status)
+
+    assert message in logs
+clear_log_file()
+
+def test_log_event_success():
+    check_log_event("user1", "success")
+
+
+def test_log_event_expired():
+    check_log_event("user2", "expired")
+
+def test_log_event_failed():
+    check_log_event("user3", "failed")
+
+
+
+
+
+
+
+    # #test expired status
+    # log_event("use2", "expired")
+    # #Test failed status
+    # log_event("use3", "failed")
+    #
+    # with open("login_system.log", "r") as file_log:
+    #      logs = file_log.read()
+    #
+    # message = get_message("user1", "success")
+    #
+    # assert message in logs
